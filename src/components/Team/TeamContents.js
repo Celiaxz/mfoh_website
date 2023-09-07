63be0392e5b2eb0909004550ffd8a08faafc1e57
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-09-07 23:39:26 +0200

    data

---
+++ src/components/Team/TeamContents.js
@@ -0,0 +1,32 @@
+export const Teamcontents = [
+  {
+    name: "Who we are",
+    image: `${photoOne}`,
+  },
+  {
+    layout: "layout2",
+    title: "What we do",
+    image: `${photoTwo}`,
+  },
+  {
+    layout: "layout3",
+    title: "",
+    image: `${logo}`,
+    texts: [
+      "Our legacy will be measured only by what we've done for “love and for\
+                    humanity.” We believe every woman has a voice and a light to create\
+                    change, and there is no better time to DARE and to be heard than RIGHT\
+                    NOW. Together with our Ambassadors, we are on a mission to light up\
+                    the dark and to leave our mark on earth. Miss Face of Humanity 2023 is\
+                    difference makers from across the world. The time is Now.",
+    ],
+  },
+  {
+    layout: "layout4",
+    title: "Our Vision",
+    texts: [
+      "A global showcase for female change-makers who believe in the power of\
+                                      Humanity to change the world.",
+    ],
+  },
+];
