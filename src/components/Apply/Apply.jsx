21fab7925b46760432569e127d3cd08ecd9e00d1
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-09-21 23:56:48 +0200

    update

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -1,1 +1,13 @@
-function applyForm(){}
+function applyForm() {
+  return (
+    <div>
+      <h1>Apply Form</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+      </form>
+    </div>
+  );
+}
