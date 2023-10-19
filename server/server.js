e93b9ff84961967ffa222145afe87a34f96066b5
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-10-20 00:07:10 +0200

    update

--- server/server.js
+++ server/server.js
@@ -4,3 +4,10 @@ const axios = require("axios");
 const app = express();
 const port = 5000;
 app.use(express.json());
+
+//Define Paystack secret key
+//const paystackSecretkey
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
