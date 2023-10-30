13f8186a0d4e6a6f9ae8f1e72fcf5fe59ef05023
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-10-30 23:52:05 +0100

    update images for team

--- src/components/Team/TeamContents.js
+++ src/components/Team/TeamContents.js
@@ -2,9 +2,10 @@ import logo from "../../assets/Assets_mfoh/logo/logo.jpg";
 import cynthia from "../../assets/Assets_mfoh/logo/Cynthia_photo.jpg";
 import Ben10 from "../../assets/Assets_mfoh/logo/Ben.jpg";
 import Zen from "../../assets/Assets_mfoh/logo/Zen.jpg";
+import Ble from "../../assets/Assets_mfoh/logo/blessing.jpg";
 export const Teamcontents = [
   {
-    image: `${Ben10}`,
+    image: `${Ble}`,
     name: "Blessing Inyang",
     description: "National Director, MFOH Nigeria",
   },
@@ -14,7 +15,7 @@ export const Teamcontents = [
     description: "Liaison Officer",
   },
   {
-    image: `${logo}`,
+    image: `${Ben10}`,
     name: "Kennedy Onyegbado",
     description: "Brand Strategist",
   },
