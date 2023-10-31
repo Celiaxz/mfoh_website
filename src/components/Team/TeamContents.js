14bf0473f954fa118c95fc28116a8ce2d7ac95e4
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-10-31 23:21:58 +0100

    update brand strategist

--- src/components/Team/TeamContents.js
+++ src/components/Team/TeamContents.js
@@ -6,7 +6,7 @@ import Ble from "../../assets/Assets_mfoh/logo/blessing.jpg";
 import beauty from "../../assets/Assets_mfoh/logo/beauty.jpg";
 import diogo from "../../assets/Assets_mfoh/logo/diogo.jpg";
 import Ken from "../../assets/Assets_mfoh/logo/Ken2.jpg";
-// import diogo from "../../assets/Assets_mfoh/logo/diogo.jpg";
+import Emma from "../../assets/Assets_mfoh/logo/emma.jpg";
 export const Teamcontents = [
   {
     image: `${Ble}`,
@@ -19,38 +19,45 @@ export const Teamcontents = [
     description: "Liaison Officer",
   },
   {
-    image: `${Ben10}`,
+    image: `${Ken}`,
     name: "Kennedy Onyegbado",
-    description: "Brand Strategist",
+    description: "Public Relations Officer ",
   },
   {
     image: `${logo}`,
     name: "Rita Chika Ezenwa",
     description: "Brand Ambassador",
   },
-  {
-    image: `${cynthia}`,
-    name: "Cynthia Eze",
-    description: "Software Development Manager",
-  },
+
   {
     image: `${beauty}`,
     name: "Winner Akpojotor",
     description: "Social Media Analyst",
   },
+
   {
-    image: `${logo}`,
-    name: "Winner Akpojotor",
-    description: "Social Media Analyst",
+    image: `${Ben10}`,
+    name: "John Orok",
+    description: "Logistics Manager",
   },
   {
-    image: `${Ken}`,
-    name: "Winner Akpojotor",
-    description: "Social Media Analyst",
+    image: `${Emma}`,
+    name: "Emmanuel Inyang",
+    description: "Event Manager",
   },
   {
     image: `${logo}`,
-    name: "Winner Akpojotor",
-    description: "Social Media Analyst",
+    name: "Kemi Kehinde Agi",
+    description: "Financial Operations Manager",
+  },
+  {
+    image: `${Zen}`,
+    name: "Donald Agbonifo",
+    description: "Brand Strategist",
+  },
+  {
+    image: `${cynthia}`,
+    name: "Cynthia Eze",
+    description: "Software Development Manager",
   },
 ];
