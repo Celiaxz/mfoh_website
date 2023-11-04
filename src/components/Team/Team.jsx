c77c8522d68373cb91823d7cd4a4ae6cb6fb414c
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-11-04 22:18:54 +0100

    refactor team styling

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -3,19 +3,23 @@ import "./Team.css";
 export default function Team() {
   return (
     <>
-      <h1 className="teamMotto">Our Fantastic Team </h1>
-      <div className="team">
-        {Teamcontents.map((teamcontent, index) => {
-          return (
-            <div className="ourTeam">
-              <div>
-                <img className="teamImage" src={teamcontent.image} />
-              </div>
-              <div className="teamName">{teamcontent.name}</div>
-              <div className="teamDesc">{teamcontent.description}</div>
-            </div>
-          );
-        })}
+      <div className="teamMain">
+        <div className="teamCont">
+          <h1 className="teamMotto">Our Fantastic Team </h1>
+          <div className="team">
+            {Teamcontents.map((teamcontent, index) => {
+              return (
+                <div className="ourTeam">
+                  <div>
+                    <img className="teamImage" src={teamcontent.image} />
+                  </div>
+                  <div className="teamName">{teamcontent.name}</div>
+                  <div className="teamDesc">{teamcontent.description}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </>
   );
