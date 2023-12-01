a04fcd978bb7bce143c7fd7805985d7b0e8f4853
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-12-01 23:54:34 +0100

    revert

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-
+// const functions = require("firebase-functions");
 const app = express();
 // const bodyParser = require("body-parser");
 const router = require("./routes");
 
 const port = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.ORIGIN || "http://127.0.0.1:5173";
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 //Parse incoming requests with JSON payloads
 // app.use(bodyParser.json());
 
@@ -27,3 +27,5 @@ app.use(router);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// exports.api = functions.https.onRequest(app);
