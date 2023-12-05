a82a5c9d059530172d64d155e0582bc128c3e007
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-12-05 16:11:42 +0100

    team page responsive

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -6,15 +6,17 @@ export default function Team() {
       <div className="teamMain">
         <div className="teamCont">
           <h1 className="teamMotto">Our Fantastic Team </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
             {Teamcontents.map((teamcontent, index) => {
               return (
-                <div className="ourTeam">
-                  <div>
-                    <img className="teamImage" src={teamcontent.image} />
+                <div key={index} className="flex items-center justify-center">
+                  <div className="ourTeam">
+                    <div>
+                      <img className="teamImage" src={teamcontent.image} />
+                    </div>
+                    <div className="teamName">{teamcontent.name}</div>
+                    <div className="teamDesc">{teamcontent.description}</div>
                   </div>
-                  <div className="teamName">{teamcontent.name}</div>
-                  <div className="teamDesc">{teamcontent.description}</div>
                 </div>
               );
             })}
