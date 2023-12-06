65a8e13e9f6bf54069730bd43f65538ad61c5876
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-12-06 21:38:52 +0100

    style about us page

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -3,32 +3,36 @@ export default function AboutUs(props) {
   const combinedClassName = `aboutUs ${layout}`;
   return (
     <div className={combinedClassName}>
-      {layout === "layout1" && (
-        <>
-          <div className="title">
-            <h1>{title}</h1>
-          </div>
-          <div className="content">
-            <div className="text">{text}</div>
-            <div className="image">
-              {image && <img src={image} alt="logo" />}
-            </div>
-          </div>
-        </>
-      )}
-      {layout === "layout2" && (
-        <>
-          <div className="image">
-            {image && <img src={image} alt="about" />}
-          </div>
-          <div className="content">
+      <div className="aboutUsContainer">
+        {layout === "layout1" && (
+          <>
             <div className="title">
               <h1>{title}</h1>
             </div>
-            <div className="text">{text}</div>
-          </div>
-        </>
-      )}
+            <div className="contentone">
+              <div className="textone">{text}</div>
+              <div className="image">
+                {image && <img src={image} alt="logo" />}
+              </div>
+            </div>
+          </>
+        )}
+        {layout === "layout2" && (
+          <>
+            <div className="contenttwo">
+              <div className="image">
+                {image && <img src={image} alt="about" />}
+              </div>
+              <div className="title2text2">
+                <div className="title">
+                  <h1>{title}</h1>
+                </div>
+                <div className="texttwo">{text}</div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
       {layout === "layout3" && (
         <>
           <div className="image">
