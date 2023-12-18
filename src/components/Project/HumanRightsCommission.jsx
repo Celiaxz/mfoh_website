6e5a739db37be8267b9d85a70cf28d525e69a8a3
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-12-18 18:48:19 +0100

    update

---
+++ src/components/Project/HumanRightsCommission.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function HumanRightsCommission() {
+  return (
+    <>
+      <div>
+        <h1>Partnership with Human Rights Commission</h1>
+        Sequel to discussions during a courtesy/advocacy visit to the Executive
+        Secretary of the National Human Rights on 14th December 2022, the idea
+        to collaborate in advancing and effectively consolidating the respect
+        and enjoyment of human rights in Nigeria was hatched. Consequent upon a
+        series of engagements with the Commission, the proposed partnership
+        attained a new height evidenced by a strategic meeting with the Director
+        overseeing the Human Rights Institute; Mr. Obinna Nwakonye, and MFOH
+        Nigeria’s Ambassador; Queen Rita Chika Ezenwa in Abuja for proper Human
+        Rights exchange programmes.
+      </div>
+      <img></img>
+    </>
+  );
+}
